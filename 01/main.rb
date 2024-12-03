b0777718dbe39ec6bc18f512047aac8db599df6c
Brian Deane <brian.deane@gusto.com>
2024-12-02 23:25:36 -0500

    day 1 p2

--- 01/main.rb
+++ 01/main.rb
@@ -1,5 +1,15 @@
 lines = File.readlines('input').map &:strip
 
+# part 1
+def part1
+  pairs = lines.map(&:split)
+  list1, list2 = pairs.transpose.map { |row| row.map(&:to_i) }
+  p list1.sort.zip(list2.sort).map { |a, b| (a-b).abs }.sum
+end
+
+# part 2
 pairs = lines.map(&:split)
 list1, list2 = pairs.transpose.map { |row| row.map(&:to_i) }
-p list1.sort.zip(list2.sort).map { |a, b| (a-b).abs }.sum
+p list1
+  .map { |el| el * list2.count(el) }
+  .sum
