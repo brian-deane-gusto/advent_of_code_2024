d268ca3395014e1d0433640d5c95e3e6a4e5622a
Brian Deane <brian.deane@gusto.com>
2024-12-14 20:03:16 -0500

    day 4 p1

---
+++ 04/main.rb
@@ -0,0 +1,49 @@
+INPUT = File.readlines('input').map(&:strip).map(&:chars)
+
+DIRECTIONS = [
+  [0,1],
+  [0,-1],
+  [1,0],
+  [-1,0],
+  [1,1],
+  [1,-1],
+  [-1,1],
+  [-1,-1],
+]
+
+def valid_step?(i:, j:, direction:)
+  max_length = INPUT.first.length - 1
+  max_height = INPUT.length - 1
+  a,b = direction
+  (i+a).between?(0, max_length) && (j+b).between?(0, max_height)
+end
+
+def match_in_direction?(char:, test_word:, target:, i:, j:, direction:)
+  return true if target == test_word
+  return false unless valid_step?(i:, j:, direction:)
+
+  a,b = direction
+  i=i+a
+  j=j+b
+  char = INPUT[i][j]
+  return false unless target[test_word.length] == char
+
+  test_word = test_word + char
+  match_in_direction?(char:, test_word:, target:, i:, j:, direction:)
+end
+
+target = 'XMAS'
+count = 0
+
+INPUT.each_with_index do |line, i|
+  line.each_with_index do |char, j|
+    if char == target[0]
+      valid_steps = DIRECTIONS.filter { |direction| valid_step?(i:, j:, direction:) }
+      valid_steps.each do |direction|
+        count += 1 if match_in_direction?(char:, test_word: char, target:, i:, j:, direction:)
+      end
+    end
+  end
+end
+
+pp count
